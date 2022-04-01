ed8a3519dd417f80f03dfbc1644785ddd4711df5
mdyasenrafe <mdyasenrafe@gmail.com>
2022-04-01 17:17:57 +0600

    speaker added

--- src/Models/EventModel.js
+++ src/Models/EventModel.js
@@ -2,25 +2,27 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const EventSchema = new Schema({
-    eventName: {
-        type: String,
-        require: true
-    },
-    eventType: {
-        type: String,
-        require: true
-    },
-    eventDescription: {
-        type: String,
-        require: true
-    },
-    eventDate: {
-        type: Date,
-        require: true
-    }
-
-})
-
+  eventName: {
+    type: String,
+    require: true,
+  },
+  eventType: {
+    type: String,
+    require: true,
+  },
+  eventDescription: {
+    type: String,
+    require: true,
+  },
+  eventDate: {
+    type: Date,
+    require: true,
+  },
+  speaker: {
+    type: Date,
+    require: true,
+  },
+});
 
 const EventModel = mongoose.model("event", EventSchema);
 module.exports = EventModel;
