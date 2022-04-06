850e5410d366d0ae2d0e3f18949ce6d9ff6ba434
mdyasenrafe <mdyasenrafe@gmail.com>
2022-04-06 22:03:42 +0600

    cart controllers done

--- src/Controllers/CartControllers.js
+++ src/Controllers/CartControllers.js
@@ -18,18 +18,21 @@ exports.addCart = async (req, res) => {
 };
 
 exports.getCart = async (req, res) => {
-  CartModel.find({}, (err, data) => {
-    if (err) {
-      res.status(400).json({
-        error: true,
-        message: err,
-      });
-    } else {
-      res.status(200).json({
-        error: false,
-        data: data,
-        message: "data successfully",
-      });
+  CartModel.find(
+    { userPhoneNumber: req.userPhoneNumber, _id: req.id },
+    (err, data) => {
+      if (err) {
+        res.status(400).json({
+          error: true,
+          message: err,
+        });
+      } else {
+        res.status(200).json({
+          error: false,
+          data: data,
+          message: "data successfully",
+        });
+      }
     }
-  });
+  );
 };
--- src/Controllers/DashboardControllers.js
+++ src/Controllers/DashboardControllers.js
@@ -1,18 +1,21 @@
 const DashboardModel = require("../Models/DashboardModel");
 
 exports.getDashboard = async (req, res) => {
-    DashboardModel.find({ userPhoneNumber: req.userPhoneNumber, _id: req.id }, (err, data) => {
-        if (err) {
-            res.status(400).json({
-                error: true,
-                message: err,
-            });
-        } else {
-            res.status(200).json({
-                error: false,
-                data: data,
-                message: "data fetch successfully",
-            });
-        }
-    });
+  DashboardModel.find(
+    { userPhoneNumber: req.userPhoneNumber, _id: req.id },
+    (err, data) => {
+      if (err) {
+        res.status(400).json({
+          error: true,
+          message: err,
+        });
+      } else {
+        res.status(200).json({
+          error: false,
+          data: data,
+          message: "data fetch successfully",
+        });
+      }
+    }
+  );
 };
