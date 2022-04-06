0d4abea4317ecfca14e6753e939ded0802ad7f21
mdyasenrafe <mdyasenrafe@gmail.com>
2022-04-06 22:20:14 +0600

    bug resolve

--- src/Controllers/CartControllers.js
+++ src/Controllers/CartControllers.js
@@ -19,7 +19,7 @@ exports.addCart = async (req, res) => {
 
 exports.getCart = async (req, res) => {
   CartModel.find(
-    { userPhoneNumber: req.userPhoneNumber, _id: req.id },
+    { userPhoneNumber: req.userPhoneNumber, _id: req.userId },
     (err, data) => {
       if (err) {
         res.status(400).json({
