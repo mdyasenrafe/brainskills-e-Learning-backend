e72f61cb5874685d785934277edb4dbd2ce0b1c9
mdyasenrafe <mdyasenrafe@gmail.com>
2022-04-06 21:58:04 +0600

    courrse id added

--- src/Models/CartModel.js
+++ src/Models/CartModel.js
@@ -2,34 +2,38 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CartSchema = new Schema({
-    userName: {
-        type: String,
-        required: true,
-    },
-    userId: {
-        type: Number,
-        required: true,
-    },
-    userPhoneNumber: {
-        type: Number,
-        required: true,
-    },
-    CourseName: {
-        type: String,
-        required: true,
-    },
-    coursePrice: {
-        type: Number,
-        required: true,
-    },
-    coursePhoto: {
-        type: String,
-        required: true,
-    },
-    createAt: {
-        type: Date,
-        default: new Date(),
-    },
+  userName: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: Number,
+    required: true,
+  },
+  userPhoneNumber: {
+    type: Number,
+    required: true,
+  },
+  courseId: {
+    type: Number,
+    required: true,
+  },
+  courseName: {
+    type: String,
+    required: true,
+  },
+  coursePrice: {
+    type: Number,
+    required: true,
+  },
+  coursePhoto: {
+    type: String,
+    required: true,
+  },
+  createAt: {
+    type: Date,
+    default: new Date(),
+  },
 });
 
 const CartModel = mongoose.model("cart", CartSchema);
