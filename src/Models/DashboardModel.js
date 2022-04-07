78fe3f121bd6433ea118a600d233e27d6550def5
mdyasenrafe <mdyasenrafe@gmail.com>
2022-04-07 11:26:32 +0600

    dashboard model info added

--- src/Models/DashboardModel.js
+++ src/Models/DashboardModel.js
@@ -2,34 +2,38 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const DashboardSchema = new Schema({
-    userName: {
+  userName: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: Number,
+    required: true,
+  },
+  userPhoneNumber: {
+    type: Number,
+    required: true,
+  },
+  courses: [
+    {
+      CourseName: {
         type: String,
         required: true,
-    },
-    userId: {
+      },
+      coursePrice: {
         type: Number,
         required: true,
-    },
-    userPhoneNumber: {
-        type: Number,
-        required: true,
-    },
-    CourseName: {
+      },
+      coursePhoto: {
         type: String,
         required: true,
+      },
     },
-    coursePrice: {
-        type: Number,
-        required: true,
-    },
-    coursePhoto: {
-        type: String,
-        required: true,
-    },
-    createAt: {
-        type: Date,
-        default: new Date(),
-    },
+  ],
+  createAt: {
+    type: Date,
+    default: new Date(),
+  },
 });
 
 const DashboardModel = mongoose.model("dashboard", DashboardSchema);
