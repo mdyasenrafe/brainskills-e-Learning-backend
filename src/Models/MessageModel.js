877d06bc53e978da7eae60f9b9ae284f23a72f8d
musasazib <musasazib@gmail.com>
2022-04-14 13:00:34 +0600

    Add Message Model

---
+++ src/Models/MessageModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema(
+  {
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    content: { type: String, trim: true },
+    chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
+    readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  },
+  { timestamps: true }
+);
+
+const MessageModel = mongoose.model("Message", MessageSchema);
+module.exports = MessageModel;
