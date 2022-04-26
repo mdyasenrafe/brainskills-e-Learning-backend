2e19a546a0972f3d9aa1220189ed5501502135b3
Heezol06 <shahriarrahmanheezol@gmail.com>
2022-04-26 22:49:54 +0600

    quiz

--- src/Models/QuizModel.js
+++ src/Models/QuizModel.js
@@ -1,20 +1,31 @@
 const mongoose = require("mongoose");
+const { stringify } = require("uuid");
 const Schema = mongoose.Schema;
 
 const QuizSchema = new Schema({
-    QuizQuestion: {
+    question: {
         type: String,
         required: true,
     },
-    QuizAnswer: {
-        type: String,
-        required: true,
+    isQuiz: {
+        default: true,
     },
+    AnswersText: [
+        {
+            Answers: {
+                type: String,
+                required: true,
+            },
+            isCorrect: {
+                default: false,
+            }
+        },
+    ],
     createAt: {
         type: Date,
         default: new Date(),
     },
 });
 
-const instructorModel = mongoose.model("quize", QuizSchema);
-module.exports = instructorModel;
+const quizModel = mongoose.model("quiz", QuizSchema);
+module.exports = quizModel;
